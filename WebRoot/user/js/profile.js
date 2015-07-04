a3ca088b07f29a2bc12024e422bfd065dc7daef1
Hugh <hugh_77@icloud.com>
2015-07-04 10:29:52 +0800

    change .html into .jsp

---
+++ WebRoot/user/js/profile.js
@@ -0,0 +1,14 @@
+﻿function sure(){
+	alert("确认更改？")
+	var head=document.getElementById("pic").value
+	var sex=document.getElementsByName("gender").value
+	var userpassword=document.getElementById("password").value
+	var email=document.getElementById("email").value
+	var tel=document.getElementById("tel").value
+	var _tel = document.getElementById("tel");
+	_tel = tel;
+}
+function cancel(){
+	alert("确认取消？")
+}
+
